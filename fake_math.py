b0af1260cdb3fcf0801685b7889e8d5282a9aac9
RiTmm94 <shabalin.kostya2015@yandex.ru>
2024-07-17 08:53:16 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,13 @@
+def devide (first, second):
+    if second == 0:
+        return 'Ошибка, деление на ноль'
+    else:
+        a = first / second
+        return a
+
+#result1 = devide(10, 2)
+#result2 = devide(5, 0)
+#result3 = devide(0, 5)
+#print(result1)
+#print(result2)
+#print(result3)
---
+++ true_math.py
@@ -0,0 +1,15 @@
+from math import inf
+
+def devide (first, second):
+    if second == 0:
+        return inf
+    else:
+        a = first / second
+        return a
+
+#result1 = devide(10, 2)
+#result2 = devide(5, 0)
+#result3 = devide(0, 5)
+#print(result1)
+#print(result2)
+#print(result3)
